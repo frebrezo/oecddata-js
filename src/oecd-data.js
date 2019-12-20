43638844bb174344e973776a3e8ec74389fd9810
Won Kim <kimwon@iit.edu>
2019-12-20 16:21:09 +0000

    Refactored oecd-service.

---
+++ src/oecd-data.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var oecdService = require('./oecd-service');
+
+function OECDData() {
+    const oecdServiceObj = new oecdService.OECDService();
+
+    this.getDataSet = function (dataSetId) {
+        return oecdServiceObj.getDataSet(dataSetId);
+    }
+
+    this.getSeriesKeyId = function (seriesKey, dimensions) {
+        var seriesIdArr = seriesKey.split(':');
+        var newSeriesKey = '';
+        for (var i = 0; i < seriesIdArr.length; ++i) {
+            if (newSeriesKey.length > 0) newSeriesKey += ':';
+            newSeriesKey += dimensions.series[i].values[seriesIdArr[i]].id;
+        }
+        return newSeriesKey;
+    }
+
+    this.getSeriesKeyName = function (seriesKey, dimensions) {
+        var seriesIdArr = seriesKey.split(':');
+        var newSeriesKey = '';
+        for (var i = 0; i < seriesIdArr.length; ++i) {
+            if (newSeriesKey.length > 0) newSeriesKey += ':';
+            newSeriesKey += dimensions.series[i].values[seriesIdArr[i]].name;
+        }
+        return newSeriesKey;
+    }
+
+    this.getSeriesKeyIds = function(dataSet) {
+        var respJson = dataSet;
+        console.log(respJson.header.id);
+        var seriesKeyDict = {};
+        var seriesKeyId = null;
+        for (var seriesKey in respJson.dataSets[0].series) {
+            seriesKeyId = this.getSeriesKeyId(seriesKey, respJson.structure.dimensions);
+            seriesKeyDict[seriesKey] = seriesKeyId;
+        }
+        return seriesKeyDict;
+    }
+
+    this.getSeriesKeyNames = function(dataSet) {
+        var respJson = dataSet;
+        console.log(respJson.header.id);
+        var seriesKeyDict = {};
+        var seriesKeyName = null;
+        for (var seriesKey in respJson.dataSets[0].series) {
+            seriesKeyName = this.getSeriesKeyName(seriesKey, respJson.structure.dimensions);
+            seriesKeyDict[seriesKey] = seriesKeyName;
+        }
+        return seriesKeyDict;
+    }
+}
+
+module.exports.OECDData = OECDData;
+
+var dataSetId = 'HH_DASH';
+var oecdDataObj = new OECDData();
+var requestPromise = oecdDataObj.getDataSet(dataSetId);
+//https://javascript.info/promise-basics
+requestPromise.then(
+    result => {
+        var respJson = result;
+        console.log(respJson.header.id);
+        var seriesKeyId = oecdDataObj.getSeriesKeyIds(respJson);
+        var seriesKeyName = oecdDataObj.getSeriesKeyNames(respJson);
+        for (var seriesKey in seriesKeyId) {
+            console.log(seriesKey + ', ' + seriesKeyId[seriesKey] + ', ' + seriesKeyName[seriesKey]);
+        }
+    },
+    error => {
+        console.error('PANIC! Something is very wrong here.');
+    });
--- src/oecd-service.js
+++ src/oecd-service.js
@@ -1,54 +1,24 @@
+'use strict';
+
 var request = require('request');
 
-var getSeriesKeyId = function (seriesKey, dimensions) {
-    var seriesIdArr = seriesKey.split(':');
-    var newSeriesKey = '';
-    for (var i = 0; i < seriesIdArr.length; ++i) {
-        if (newSeriesKey.length > 0) newSeriesKey += ':';
-        newSeriesKey += dimensions.series[i].values[seriesIdArr[i]].id;
-    }
-    return newSeriesKey;
-}
+function OECDService() {
+    this.getDataSet = function (dataSetId) {
+        var path = '/SDMX-JSON/data/' + dataSetId;
+        console.log(path);
 
-var getSeriesKeyName = function (seriesKey, dimensions) {
-    var seriesIdArr = seriesKey.split(':');
-    var newSeriesKey = '';
-    for (var i = 0; i < seriesIdArr.length; ++i) {
-        if (newSeriesKey.length > 0) newSeriesKey += ':';
-        newSeriesKey += dimensions.series[i].values[seriesIdArr[i]].name;
-    }
-    return newSeriesKey;
-}
-
-var processDataSet = function (dataSetId) {
-    var path = '/SDMX-JSON/data/' + dataSetId;
-    console.log(path);
-
-    var requestPromise = new Promise((resolve, reject) => {
-        request('https://stats.oecd.org' + path,
-            function (error, response, body) {
-                console.error('error:', error); // Print the error if one occurred
-                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                resolve(body);
-            });
-    });
-
-    //https://javascript.info/promise-basics
-    requestPromise.then(
-        result => {
-            console.log(result);
-            respJson = JSON.parse(result);
-            console.log(respJson.header.id);
-            for (seriesKey in respJson.dataSets[0].series) {
-                var seriesKeyId = getSeriesKeyId(seriesKey, respJson.structure.dimensions);
-                var seriesKeyName = getSeriesKeyName(seriesKey, respJson.structure.dimensions);
-                console.log(seriesKey + ', ' + seriesKeyId + ', ' + seriesKeyName);
-            }
-        },
-        error => {
-            console.error('PANIC! Something is very wrong here.');
+        var requestPromise = new Promise((resolve, reject) => {
+            request('https://stats.oecd.org' + path,
+                function (error, response, body) {
+                    console.error('error:', error); // Print the error if one occurred
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    console.log(body);
+                    resolve(JSON.parse(body));
+                });
         });
+
+        return requestPromise;
+    }
 }
 
-var dataSetId = 'HH_DASH';
-processDataSet(dataSetId);
+module.exports.OECDService = OECDService;
