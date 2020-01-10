631178e660a26da22cdd0cc1dd3c74e80ba0eb62
Won Kim <wkim1@lenovo.com>
2020-01-10 10:01:50 -0800

    Added /:dataSetId and /:dataSetId/metadata paths.

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const OECDData = require('./oecd-data');
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
+});
+
+app.get('/:dataSetId', (req, res) => {
+    res.send('Retrieving ' + req.params.dataSetId);
+});
+
+app.get('/:dataSetId/metadata', (req, res) => {
+    var dataSetId = req.params.dataSetId;
+    var oecdData = new OECDData();
+    var dataSetPromise = oecdData.getDataSet(dataSetId);
+    //https://javascript.info/promise-basics
+    dataSetPromise.then(
+        dataSet => {
+            var dimensions = oecdData.getDimensions(dataSet);
+            res.send(dimensions);
+        }, error => {
+            console.error('PANIC! Something is very wrong here.');
+        });
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
