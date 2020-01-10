00cade922a7e555993bbc84e4033dbe4839fb7a9
Won Kim <wkim1@lenovo.com>
2020-01-10 11:22:01 -0800

    Refactored oecdData to global variable.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const OECDData = require('./oecd-data');
 
+var oecdData = new OECDData();
+
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
@@ -10,14 +12,23 @@ app.get('/:dataSetId', (req, res) => {
     res.send('Retrieving ' + req.params.dataSetId);
 });
 
-app.get('/:dataSetId/metadata', (req, res) => {
+app.get('/:dataSetId/metadata/dimensions', (req, res) => {
     var dataSetId = req.params.dataSetId;
-    var oecdData = new OECDData();
     var dataSetPromise = oecdData.getDataSet(dataSetId);
     //https://javascript.info/promise-basics
     dataSetPromise.then(result => {
         var dimensions = oecdData.getDimensions(result);
         res.send(dimensions);
+    });
+});
+
+app.get('/:dataSetId/metadata/attributes', (req, res) => {
+    var dataSetId = req.params.dataSetId;
+    var dataSetPromise = oecdData.getDataSet(dataSetId);
+    //https://javascript.info/promise-basics
+    dataSetPromise.then(result => {
+        var attributes = oecdData.getAttributes(result);
+        res.send(attributes);
     });
 });
 
